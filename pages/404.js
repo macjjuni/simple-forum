63616f85f1bb2163717dcf2641d57a05dffeb78d
ju_ni <38034518+junheeleeme@users.noreply.github.com>
2022-02-08 00:23:01 +0900

    feat: fixed error page

--- pages/404.js
+++ pages/404.js
@@ -9,7 +9,7 @@ export default function Error({ statusCode }) {
     <>
         <HeadInfo title='404 NOT FOUND'></HeadInfo>
         <div className="404-wrap h-[500px] flex flex-col justify-center items-center">
-            <h1 className="block text-xl">{statusCode} Error - Page Not Found</h1>
+            <h1 className="block text-3xl mb-5">Page Not Found</h1>
             <button onClick={(()=> push('/'))} className="block my-5 px-5 py-2 bg-blue-400 rounded-sm">
                 Go Home
             </button>
@@ -19,9 +19,3 @@ export default function Error({ statusCode }) {
     </>
     )
 }
-
-Error.getInitialProps = ({res, err}) => {
-    const statusCode = res ? res.statusCode : err ? err.statusCode : 404
-    return { statusCode }
-}
-
